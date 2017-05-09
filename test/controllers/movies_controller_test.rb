58b2457c0451893952c1a0967d28e4e1dcc714e8
Haby Randall <sow.haby@gmail.com>
2017-05-09 14:35:38 -0700

    Index tests for movies controller

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 describe MoviesController do
 
 
-KEYS = %w(title overview release_date inventory available_inventory)
+KEYS = %w(title overview release_date inventory available_inventory).sort
 
 describe "index" do
   it "is a working route" do
@@ -21,6 +21,23 @@ describe "index" do
 
     body = JSON.parse(response.body)
     body.must_be_kind_of Array
+  end
+
+  it "returns all of the movies" do
+    get movies_url
+
+    body = JSON.parse(response.body)
+
+    body.length.must_equal Movie.count
+  end
+
+  it "returns movies with the required fields" do
+    get movies_url
+    body = JSON.parse(response.body)
+
+    body.each do |movie|
+      movie.keys.sort.must_equal KEYS
+    end
   end
 
 end
