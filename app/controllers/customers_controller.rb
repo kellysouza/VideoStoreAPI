dfd6828c90fbf45a8b222c1106d3b61c3658a6e1
Haby Randall <sow.haby@gmail.com>
2017-05-09 15:37:54 -0700

    Writing tests for customer controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,7 @@
 class CustomersController < ApplicationController
+
+  def index
+    customers = Customer.all
+    render json: customers.as_json(only: [:name, :address, :city, :state, :postal_code, :phone, :registered_at, :account_credit])
+  end
 end
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,41 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+
+  CUST_KEYS = %w(name address city state postal_code phone registered_at account_credit).sort
+
+  describe "index" do
+    it "is a real working route" do
+      get customers_url
+      must_respond_with :success
+    end
+  end
+
+  it 'returns json' do
+    get customers_url
+    response.header["Content-Type"].must_include 'json'
+  end
+
+  it "returns an array" do
+    get customers_url
+
+    body = JSON.parse(response.body)
+    body.must_be_kind_of Array
+  end
+
+  it "returns all the customers" do
+    get customers_url
+
+    body = JSON.parse(response.body)
+    body.length.must_equal Customer.count
+  end
+
+  it "returns customer with the required fields" do
+    get customers_url
+    body = JSON.parse(response.body)
+
+    body.each do |customer|
+      customer.keys.sort.must_equal CUST_KEYS
+    end
+  end
 end
