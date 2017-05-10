4bbbc4ef2767783db0de1d42fb77580b1dc761fe
Haby Randall <sow.haby@gmail.com>
2017-05-10 16:01:02 -0700

    Working on create test in rentals

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 describe RentalsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  KEYS = %w(customer_id movie_id due_date)
+
+  describe "create" do
+    let(:rental_data) {
+      {
+        customer_id: 1,
+        movie_id: (movies(:one)).id,
+        due_date: "20170609",
+        title: "time"
+      }
+    }
+    it "creates a new rental with valid data" do
+      proc {
+        post checkout_path("title"), params: { rental: rental_data }
+      }.must_change 'Rental.count', 1
+      must_respond_with :ok
+    end
+  end
 end
